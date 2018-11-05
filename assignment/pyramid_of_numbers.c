4c7164085da0d07adb8299bc1b56dadc2da62bed
polleichtnermoritz <moritz.polleichtner@outlook.com>
2018-11-05 21:36:53 +0100

    without divide

--- assignment/pyramid_of_numbers.c
+++ assignment/pyramid_of_numbers.c
@@ -1,9 +1,9 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Class: <your class>
+ *				HTBLA-Leonding / Class: 2AHIF
  * ---------------------------------------------------------
- * Exercise Number: 0
+ * Exercise Number: 5
  * Title:			Pyramid of Numbers
- * Author:			<your name>
+ * Author:			moritz Polleichtner
  * ----------------------------------------------------------
  * Description:
  * Calculates a pyramid of numbers, i.e., it multiplies a big
@@ -13,6 +13,7 @@
  * ----------------------------------------------------------
  */
 #include <stdio.h>
+#include <string.h>
 
 /// The maximum number of digits allowed in a big int.
 #define MAX_DIGITS 80
@@ -76,5 +77,72 @@ void copy_big_int(const struct BigInt *from, struct BigInt *to);
 */
 int main(int argc, char *argv[])
 {
+	char input[MAX_DIGITS];
+	struct BigInt big_int;
+
+	printf("Please enter a number: ");
+	scanf("%s",input);
+
+	big_int.digits_count = strtobig_int(input,strlen(input),&big_int);
+
+	if(big_int.digits_count==strlen(input))
+	{
+		for (int i = 2; i <= 9; i++) {
+			print_big_int(&big_int);
+			multiply(&big_int,i,&big_int);
+			printf(" * %d = ",i);
+			print_big_int(&big_int);
+			printf("\n");
+		}
+	}
 	return 0;
+}
+
+int strtobig_int(const char *str, int len, struct BigInt *big_int)
+{
+	int count=0;
+
+	for (int i = len-1; i >=0 ; i--)
+	{
+		if(str[i]>='0'&&str[i]<='9')
+		{
+			big_int->the_int[count]=str[i]-'0';
+			count++;
+		}
+		else
+		{
+			return count;
+		}
+	}
+	return count;
+}
+
+void print_big_int(const struct BigInt *big_int)
+{
+	for (int i = big_int->digits_count-1; i >= 0; i--) {
+		printf("%d",big_int->the_int[i]);
+	}
+}
+
+void multiply(const struct BigInt *big_int, int factor, struct BigInt *big_result)
+{
+	int overflow=0;
+	int num;
+	for (int i = 0; i <big_int->digits_count; i++)
+	{
+		num=big_int->the_int[i]*factor + overflow;
+		big_result->the_int[i]=num%10;
+
+		if(num>=10)
+		{
+			overflow=num/10;
+		}
+		else{
+			overflow=0;
+		}
+		if(overflow!=0 && i==big_int->digits_count-1)
+		{
+			big_result->digits_count++;
+		}
+	}
 }
